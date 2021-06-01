00b9fd9139157089b549b23b93d8cf3ca83452eb
Pradeep Mahato <65070817+pradeep-SSB@users.noreply.github.com>
2021-06-02 01:18:04 +0530

    Add files via upload
    LinkedList

---
+++ src/Linked_List/remove_duplicates.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Program {
+  // This is an input class. Do not edit.
+  public static class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+    // Write your code here.
+    return null;
+  }
+}
