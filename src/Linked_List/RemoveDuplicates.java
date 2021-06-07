04cb1537fce282c879f09eb8d18e3a2208ca3e65
Pradeep Mahato <65070817+pradeep-SSB@users.noreply.github.com>
2021-06-08 01:11:31 +0530

    Easy
    Problem - 1

---
+++ src/Linked_List/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+// Remove Duplicates where values are already sorted.
+// O(N) && S(1)
+
+class Program {
+  // This is an input class. Do not edit.
+  public static class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+    // Write your code here.
+		LinkedList CurrentNode = linkedList;
+			while(CurrentNode != null){
+				LinkedList NextDistinctNode =  CurrentNode;
+					while(NextDistinctNode != null && NextDistinctNode.value == CurrentNode.value){
+						NextDistinctNode = NextDistinctNode.next;
+					}
+				CurrentNode.next = NextDistinctNode;
+				CurrentNode = NextDistinctNode;
+			}
+		
+    return linkedList;
+  }
