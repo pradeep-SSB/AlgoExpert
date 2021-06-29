58b7fdbe4199bf04705f75ab0a649df45576dd05
Pradeep Mahato <65070817+pradeep-SSB@users.noreply.github.com>
2021-06-30 01:43:52 +0530

    Add files via upload
    Problem- 5

---
+++ src/Linked_List/Find_Loop_and_return_loopNode.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Program {
+  public static LinkedList findLoop(LinkedList head) {
+    // Write your code here.
+		LinkedList tortoise = head.next;  //key : initilization
+		LinkedList hare = head.next.next;
+		while(tortoise != hare){       // detects loop
+			tortoise = tortoise.next;
+			hare = hare.next.next;
+		}
+		tortoise = head;
+		while(tortoise != hare){	// mathematically returns the LoopNode 
+			tortoise = tortoise.next;
+			hare = hare.next;
+		}
+    return tortoise;
+  }
+
+  static class LinkedList {
+    int value;
+    LinkedList next = null;
+
+    public LinkedList(int value) {
+      this.value = value;
+    }
+  }
+}
